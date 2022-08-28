ba4b73487e4be798c9fc357cc1c6849284977f56
SamuraiZen28 <84150236+SamuraiZen28@users.noreply.github.com>
2022-08-28 10:06:09 -0400

    Update global.h

--- rdr2 scripting environment/samples/Pools/global.h
+++ rdr2 scripting environment/samples/Pools/global.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include "keyboard.h"
 
+using namespace std;
+
 inline Hash joaat(const char* text)
 {
 	return MISC::GET_HASH_KEY(text);
@@ -14,6 +16,41 @@ Ped pwo = 0;
 Vehicle pwov = 0;
 Ped pwoh = 0;
 Ped pwod = 0;
+Blip iVar0;
+//Create A Ped Vector To Store All You Spawned Peds.
+vector<Ped> pedVector;
+vector<Vehicle> vehicleVector;
+/*After creating the ped in the function, pass in pedVector.push_back(ped);*/
+
+/*You can clear the vectors with pedVector.clear();
+If you want to remove the dead peds from a vector, do this.
+for (int i = 0; i < pedVector.size(); i++)
+{
+	if (ENTITY::IS_ENTITY_DEAD(pedVector[i]))
+	{
+		pedVector.erase(pedVector.begin() + i);
+	}
+}
+*/
+
+//Adding blips to entities.
+//create your variable i.e. Blip iVar0; as seen above.
+//if you want to add the blip to a specific entity, which will move with them, 
+/*iVar0 = MAP::BLIP_ADD_FOR_ENTITY(BLIP_STYLE_ENEMY, pedToPutBlipOn);
+MAP::SET_BLIP_SPRITE(iVar0, joaat("blip_ambient_companion"), true);
+MAP::_SET_BLIP_NAME_FROM_PLAYER_STRING(iVar0, "Enemy");
+MAP::BLIP_ADD_MODIFIER(iVar0, BLIP_MODIFIER_ENEMY);
+MAP::SET_GPS_MULTI_ROUTE_RENDER(true);
+-----------------------------------------------------------------------------------------
+if you'd want to put them on say a whole vector of peds, maybe it's an enemies vector
+for(int i = 0; i < pedVector.size(); i++)
+{
+	place the above blip code here, just replace the "pedToPutBlipOn" with pedVector[i].
+}
+now that theres blips, you'll want to remove the blips once those peds die, in the for loop right under where you remove the dead pead, put 
+MAP::REMOVE_BLIP(&iVar0);
+*/
+
 
 inline void createPedWithOptions(Hash model, float posx, float posy, float posz, float heading = 0.f, bool onHorse = false, Hash horseModel = NULL, bool spawnWithScenario = false, Hash scenarioName = NULL, float scenarioHeading = 0.f, int outfitVariation = 0, bool inVehicle = false, int seatIndex = 0, Hash VehicleName = NULL)
 {
@@ -28,6 +65,7 @@ inline void createPedWithOptions(Hash model, float posx, float posy, float posz,
 
 	pwoh = PED::CREATE_PED(horseModel, posx + 2, posy + 2, posz, heading, false, false, false, false);
 	pwod = PED::CREATE_PED_ON_MOUNT(pwoh, model, VS_DRIVER, false, false, false, false);
+	pedVector.push_back(pwoh & pwod);
 	PED::_EQUIP_PED_OUTFIT_PRESET(pwoh, outfitVariation, false);
 	PED::_UPDATE_PED_VARIATION(pwoh, true, true, true, true, true);
 	PED::_EQUIP_PED_OUTFIT_PRESET(pwod, outfitVariation, false);
@@ -46,6 +84,8 @@ inline void createPedWithOptions(Hash model, float posx, float posy, float posz,
 		pwov = VEHICLE::CREATE_VEHICLE(VehicleName, posx + 2, posy + 2, posz, heading, true, true, false, true);
 		VEHICLE::SET_VEHICLE_ON_GROUND_PROPERLY(pwov, true);
 		pwo = PED::CREATE_PED_INSIDE_VEHICLE(pwov, model, seatIndex, true, true, true);
+		vehicleVector.push_back(pwov);
+		pedVector.push_back(pwo);
 		PED::_EQUIP_PED_OUTFIT_PRESET(pwo, outfitVariation, false);
 		PED::_UPDATE_PED_VARIATION(pwo, true, true, true, true, true);
 	}
@@ -57,6 +97,7 @@ inline void createPedWithOptions(Hash model, float posx, float posy, float posz,
 		WAIT(0);
 		}
 		pwo = PED::CREATE_PED(model, posx + 5, posy + 5, posz, heading, false, false, false, false);
+		pedVector.push_back(pwo);
 		ENTITY::PLACE_ENTITY_ON_GROUND_PROPERLY(pwo, true);
 		PED::_EQUIP_PED_OUTFIT_PRESET(pwo, outfitVariation, false);
 		PED::_UPDATE_PED_VARIATION(pwo, true, true, true, true, true);
