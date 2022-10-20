8ffd34ca7f33bfa239e934a3c4029883d9412f13
SamuraiZen28 <84150236+SamuraiZen28@users.noreply.github.com>
2022-10-20 16:09:24 -0400

    Update main.cpp

--- rdr2 scripting environment/samples/Pools/main.cpp
+++ rdr2 scripting environment/samples/Pools/main.cpp
@@ -6,17 +6,20 @@
 
 #include "..\..\inc\main.h"
 #include "script.h"
+#include "keyboard.h"
 
 BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
 {
-	switch (reason)
-	{
-	case DLL_PROCESS_ATTACH:
-		scriptRegister(hInstance, ScriptMain);
-		break;
-	case DLL_PROCESS_DETACH:
-		scriptUnregister(hInstance);
-		break;
-	}		
-	return TRUE;
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        scriptRegister(hInstance, ScriptMain);
+        keyboardHandlerRegister(OnKeyboardMessage);
+        break;
+    case DLL_PROCESS_DETACH:
+        scriptUnregister(hInstance);
+        keyboardHandlerUnregister(OnKeyboardMessage);
+        break;
+    }
+    return TRUE;
 }
