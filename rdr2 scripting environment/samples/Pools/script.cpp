10ff9469532711d32db6534e6ab861458a993108
SamuraiZen28 <84150236+SamuraiZen28@users.noreply.github.com>
2022-12-03 15:13:12 -0500

    Update script.cpp

--- rdr2 scripting environment/samples/Pools/script.cpp
+++ rdr2 scripting environment/samples/Pools/script.cpp
@@ -13,26 +13,6 @@
 
 using namespace std;
 
-
-/*------------- SPAWN PED ----------------------------------------------------------------------------------------------------------------------------------------------------*/
-Ped ped = 0;
-const char* guardHorses[] = { "A_C_Horse_KentuckySaddle_Black", "A_C_Horse_KentuckySaddle_ChestnutPinto", "A_C_Horse_KentuckySaddle_Grey", "A_C_Horse_KentuckySaddle_SilverBay" };
-
-void createPed(Hash model, float posx, float posy, float posz, float heading)
-{
-	STREAMING::REQUEST_MODEL(model, true);
-	if (!STREAMING::HAS_MODEL_LOADED(model))
-	{
-		WAIT(0);
-	}
-	ped = PED::CREATE_PED(model, posx, posy, posz, heading, false, false, false, false);
-	ENTITY::PLACE_ENTITY_ON_GROUND_PROPERLY(ped, true);
-	PED::_EQUIP_PED_OUTFIT_PRESET(ped, 0, false);
-	PED::_UPDATE_PED_VARIATION(ped, true, true, true, true, true);
-}
-/*Example of spawning ped*/
-//Vector3 PlayerPos = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_ID(), true, true);
-//createPed(joaat("cs_dutch"), PlayerPos.x, PlayerPos.y + MISC::GET_RANDOM_INT_IN_RANGE(5, 10), PlayerPos.z, 0.f);
 /*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*Examples of createPedWithOptions function
 |  Spawning A Ped With A Horse  |
@@ -49,7 +29,7 @@ void main()
 {
 	while (true)
 	{
-
+		ENTITY::SET_ENTITY_INVINCIBLE(PLAYER::PLAYER_PED_ID(), true);
 		WAIT(0);
 	}
 }
